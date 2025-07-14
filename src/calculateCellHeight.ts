e8905ff7406167203df0328cfbc0e29145d35442
Alexander Gavryushenko <a_gavryushenko@wargaming.net>
2025-07-14 16:50:53 +0300

    Performance of calculateCellHeights

--- src/calculateCellHeight.ts
+++ src/calculateCellHeight.ts
@@ -1,10 +1,31 @@
-import {
-  wrapCell,
-} from './wrapCell';
+import stripAnsi from "strip-ansi";
 
 /**
  * Calculates height of cell content in regard to its width and word wrapping.
  */
 export const calculateCellHeight = (value: string, columnWidth: number, useWrapWord = false): number => {
-  return wrapCell(value, columnWidth, useWrapWord).length;
+  if (value.length === 0) {
+    return 1;
+  }
+  const strippedValue = stripAnsi(value);
+  const size = Math.max(1, columnWidth);
+
+  const pattern = useWrapWord ?
+    new RegExp(`(.{0,${size - 1}}(?:(?:.\\s|[\\\\\\/_\\.,;-]|.$))|.{0,${size}}\\s?)`, 'g') :
+    new RegExp(`(.{0,${size}}\\s*)`, 'g');
+
+  let count = 0;
+  let match;
+
+  while ((match = pattern.exec(strippedValue)) !== null) {
+    if (match[0] || match.index === strippedValue.length && strippedValue.endsWith('\n')) {
+      count++;
+    }
+
+    if (match.index === pattern.lastIndex) {
+      pattern.lastIndex++;
+    }
+  }
+
+  return count || 1;
 };
--- test/calculateRowHeights.ts
+++ test/calculateRowHeights.ts
@@ -1,6 +1,9 @@
 /* eslint-disable max-nested-callbacks */
 
 import {
+  performance,
+} from 'perf_hooks';
+import {
   expect,
 } from 'chai';
 import {
@@ -70,6 +73,32 @@ describe('calculateRowHeights', () => {
 
         expect(rowHeights[0]).to.equal(3);
       });
+    });
+  });
+  context('performance', () => {
+    it('processes 1000 rows with 100 newlines each in under 100 ms', () => {
+      const data = Array.from({length: 1_000}, () => {
+        return ['!', '?\n'.repeat(100)];
+      });
+
+      const config = makeTableConfig(data, {
+        columns: {
+          0: {
+            width: 10,
+            wrapWord: false,
+          },
+        },
+      });
+
+      const startTime = performance.now();
+      const rowHeights = calculateRowHeights(data, config);
+      const endTime = performance.now();
+
+      const executionTime = endTime - startTime;
+
+      expect(executionTime).to.be.lessThan(50);
+      expect(rowHeights).to.have.length(1_000);
+      expect(rowHeights[0]).to.equal(101);
     });
   });
 });
